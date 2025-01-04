b23e3e3aaca9eb572213534556e48f2fc463f500
Lejla Muratovic <lejlamuratovic6@gmail.com>
2025-01-04 18:07:14 +0100

    dotenv fix

--- src/main/java/ba/edu/ibu/budgettracker/BudgetTrackerApplication.java
+++ src/main/java/ba/edu/ibu/budgettracker/BudgetTrackerApplication.java
@@ -8,12 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BudgetTrackerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		try {
+			// Attempt to load the .env file
+			Dotenv dotenv = Dotenv.configure().load();
+			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		} catch (Exception e) {
+			// Fallback if .env is not found
+			System.out.println("Could not load .env file. Falling back to system environment variables.");
+		}
 
 		SpringApplication.run(BudgetTrackerApplication.class, args);
-
-
 	}
-
 }
